bce78a58f85bac0ae0d686fdba2dd2523912b17d
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-01-08 23:34:46 +0500

    fix: fix isIsoscelesTriangle function test bugs

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -97,6 +97,9 @@ function isIsoscelesTriangle(a, b, c) {
   if (a <= 0 || b <= 0 || c <= 0) {
     return false;
   }
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false;
+  }
   return a === b || a === c || b === c;
 }
 
